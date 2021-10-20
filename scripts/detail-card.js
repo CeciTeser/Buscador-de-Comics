5d7b970499fcf1dba7754fa1ba59fc626bc4c748
CeciTeser <mcteser@gmai.com>
2021-10-20 18:52:05 +0200

    tipado

--- scripts/detail-card.js
+++ scripts/detail-card.js
@@ -1,90 +1,78 @@
-const detail = document.getElementById('description-detail');
-const error = document.getElementById('error');
-const containerCards = document.querySelector('#container-cards');
-const titleResults = document.getElementById('title-results');
-const totalResults = document.getElementById('total-results');
-const params = new URLSearchParams(window.location.search);
-const limit = 20;
-const page = params.get('page') == undefined ? 1 : parseInt(params.get('page'));
-const offset = (page - 1) * limit;
-const formSearch = document.getElementById('form-search');
-const orderSelect = document.getElementById('order-select');
-const typeSelect = document.getElementById('type-select');
-const imgHeaderDetail = document.getElementById('img-header-detail');
-
-const fetchDetail = () => {
-    const type = params.get('type');
-    const id = params.get('id');
-    let content = '';
-    if(type == 'comic'){
-        fetchComicById(id).then(
-            (comic) => {
-                showDetailByComic(comic.title, comic.thumbnailUrl, comic.modified, comic.description, comic.creators);
-                fetchCharactersByComic(id, offset, limit).then(
-                    (charactersResponse) => {
-                        for (const character of charactersResponse.characters) {
-                            const cell = getCellHTML('cards-characters', `./detail-card.html?id=${character.id}&type=character`, character.thumbnailUrl, character.name,  character.name);
-                            content += cell;
-                        }
-                        titleResults.innerHTML = 'Personajes';
-                        totalResults.innerHTML = charactersResponse.total;
-                        containerCards.innerHTML = content;
-                        displayPaged(charactersResponse.total, limit, page, 'detail-card.html');
-                    }, 
-                    (error) => {
-                        showErrorMessage();
-                    }
-                );
-                imgHeaderDetail.classList.add('img-header-detail-comic');
-            }, 
-            (error) => {
+// ------------------VARIABLES-------------------------
+var detail = document.getElementById('description-detail');
+var error = document.getElementById('error');
+var containerCards = document.querySelector('#container-cards');
+var titleResults = document.getElementById('title-results');
+var totalResults = document.getElementById('total-results');
+var params = new URLSearchParams(window.location.search);
+var limit = 20;
+var page = params.get('page') == undefined ? 1 : parseInt(params.get('page'));
+var offset = (page - 1) * limit;
+var formSearch = document.getElementById('form-search');
+var orderSelect = document.getElementById('order-select');
+var typeSelect = document.getElementById('type-select');
+var imgHeaderDetail = document.getElementById('img-header-detail');
+var imageDetail = document.getElementById('image-detail');
+// ------------------FETCH DETAIL-------------------------
+var fetchDetail = function () {
+    var type = params.get('type');
+    var id = params.get('id');
+    var content = '';
+    if (type == 'comic') {
+        fetchComicById(id).then(function (comic) {
+            showDetailByComic(comic.title, comic.thumbnailUrl, comic.modified, comic.description, comic.creators);
+            fetchCharactersByComic(id, offset, limit).then(function (charactersResponse) {
+                for (var _i = 0, _a = charactersResponse.characters; _i < _a.length; _i++) {
+                    var character = _a[_i];
+                    var cell = getCellHTML('cards-characters', "./detail-card.html?id=" + character.id + "&type=character", character.thumbnailUrl, character.name, character.name);
+                    content += cell;
+                }
+                titleResults.innerHTML = 'Personajes';
+                totalResults.innerHTML = charactersResponse.total;
+                containerCards.innerHTML = content;
+                displayPaged(charactersResponse.total, limit, page, 'detail-card.html');
+            }, function (error) {
                 showErrorMessage();
-            } 
-        );
-      
-    }else if(type == 'character'){
-        fetchCharacterById(id).then(
-            (character) => {
-                showDetailByCharacter(character.name, character.thumbnailUrl, character.description);
-                fetchComicsByCharacter(id, offset, limit).then(
-                    (comicsResponse) => {
-                        for(const comic of comicsResponse.comics){
-                            const cell = getCellHTML('cards-comics', `./detail-card.html?id=${comic.id}&type=comic`, comic.thumbnailUrl, comic.title,  comic.title);
-                            content += cell;
-                        }
-                        titleResults.innerHTML = 'Comics';
-                        totalResults.innerHTML = comicsResponse.total;
-                        containerCards.innerHTML = content;
-                        displayPaged(comicsResponse.total, limit, page, 'detail-card.html');
-                    },
-                    (error) => {
-                        showErrorMessage();
-                    }
-                );
-                imgHeaderDetail.classList.add('img-header-detail-character');
-            }, 
-            (error) => {
-                showErrorMessage();
-            }
-        );
-        
+            });
+            imgHeaderDetail.classList.add('img-header-detail-comic');
+        }, function (error) {
+            showErrorMessage();
+        });
     }
-}
-
-const imageDetail = document.getElementById('image-detail');
-
-const showDetailByComic = (title, thumbnailUrl, modified, description, creators) => {
-    const detailComic = document.getElementById('description-detail');
-    const titleDetail = document.createElement('h2');
-    const hPublishedDetail = document.createElement('h3');
-    const pPublishedDetail = document.createElement('p');
-    const hWritersDetail = document.createElement('h3');
-    const pWritersDetail = document.createElement('p');
-    const hDescriptionDetail = document.createElement('h3');
-    const pDescriptionDetail = document.createElement('p');
+    else if (type == 'character') {
+        fetchCharacterById(id).then(function (character) {
+            showDetailByCharacter(character.name, character.thumbnailUrl, character.description);
+            fetchComicsByCharacter(id, offset, limit).then(function (comicsResponse) {
+                for (var _i = 0, _a = comicsResponse.comics; _i < _a.length; _i++) {
+                    var comic = _a[_i];
+                    var cell = getCellHTML('cards-comics', "./detail-card.html?id=" + comic.id + "&type=comic", comic.thumbnailUrl, comic.title, comic.title);
+                    content += cell;
+                }
+                titleResults.innerHTML = 'Comics';
+                totalResults.innerHTML = comicsResponse.total;
+                containerCards.innerHTML = content;
+                displayPaged(comicsResponse.total, limit, page, 'detail-card.html');
+            }, function (error) {
+                showErrorMessage();
+            });
+            imgHeaderDetail.classList.add('img-header-detail-character');
+        }, function (error) {
+            showErrorMessage();
+        });
+    }
+};
+// ------------------SHOW DETAIL BY COMIC-------------------------
+var showDetailByComic = function (title, thumbnailUrl, modified, description, creators) {
+    var detailComic = document.getElementById('description-detail');
+    var titleDetail = document.createElement('h2');
+    var hPublishedDetail = document.createElement('h3');
+    var pPublishedDetail = document.createElement('p');
+    var hWritersDetail = document.createElement('h3');
+    var pWritersDetail = document.createElement('p');
+    var hDescriptionDetail = document.createElement('h3');
+    var pDescriptionDetail = document.createElement('p');
     imageDetail.src = thumbnailUrl;
     imageDetail.alt = title;
-
     titleDetail.innerHTML = title;
     hPublishedDetail.innerHTML = 'Publicado: ';
     pPublishedDetail.innerHTML = modified.toLocaleDateString();
@@ -92,7 +80,6 @@ const showDetailByComic = (title, thumbnailUrl, modified, description, creators)
     pWritersDetail.innerHTML = creators.join(', ') || '';
     hDescriptionDetail.innerHTML = 'Descripción';
     pDescriptionDetail.innerHTML = description || '';
-
     detailComic.appendChild(titleDetail);
     detailComic.appendChild(hPublishedDetail);
     detailComic.appendChild(pPublishedDetail);
@@ -100,33 +87,27 @@ const showDetailByComic = (title, thumbnailUrl, modified, description, creators)
     detailComic.appendChild(pWritersDetail);
     detailComic.appendChild(hDescriptionDetail);
     detailComic.appendChild(pDescriptionDetail);
-}
-
-const showDetailByCharacter = (name, thumbnailUrl, description) => {
-    const detailCharacter = document.getElementById('description-detail');
-    const titleDetail = document.createElement('h2');
-    const pDescriptioDetail = document.createElement('p');
+};
+// ------------------SHOW DETAIL BY CHARACTER-------------------------
+var showDetailByCharacter = function (name, thumbnailUrl, description) {
+    var detailCharacter = document.getElementById('description-detail');
+    var titleDetail = document.createElement('h2');
+    var pDescriptioDetail = document.createElement('p');
     imageDetail.src = thumbnailUrl;
     imageDetail.alt = name;
     titleDetail.innerHTML = name;
     pDescriptioDetail.innerHTML = description;
-
     detailCharacter.appendChild(titleDetail);
     detailCharacter.appendChild(pDescriptioDetail);
-}
-
-const showErrorMessage = () => {
+};
+var showErrorMessage = function () {
     error.innerHTML = messageError('Algo salió mal, por favor intenta más tarde.');
     detail.innerHTML = '';
     containerCards.innerHTML = '';
     imageDetail.src = '';
     imageDetail.alt = '';
     document.querySelector('.results-numbers').innerHTML = '';
-    
-}
-
-
-typeSelect.addEventListener('change',  () => {buildOrderBySelectByType(formSearch.typeselect.value)});
-
+};
+typeSelect.addEventListener('change', function () { buildOrderBySelectByType(formSearch.typeselect.value); });
 fetchDetail();
 buildOrderBySelectByType(formSearch.typeselect.value);
---
+++ scripts/utils.ts
@@ -0,0 +1,145 @@
+// ------------------VARIABLES-------------------------
+
+const params = new URLSearchParams(window.location.search);
+const orderSelect = document.getElementById('order-select');
+
+// ------------------ LOADER -------------------------
+
+let contenedor = document.getElementById('loader-container');
+
+const hideLoader = () => {
+    contenedor.classList.add('hidden')
+    console.log('hide')
+}
+
+const showLoader = () => {
+    contenedor.classList.remove('hidden')
+    console.log('show')
+}
+
+// ------------------ ASSEMBLE CARD -------------------------
+const getCellHTML = (classCard:string, url:string, thumbnailUrl:string, alt:string, title:string) =>{
+    const cellHTML = "<div class=\"" + classCard + "\"><a href=\"" + url + "\"><img src=\"" + thumbnailUrl + "\" alt=\"" + alt + "\"></a><h3>" + title + "</h3></div>";
+    
+    return cellHTML;
+};
+
+// ------------------PAGINATION-------------------------
+const getPageLi = (page, content:string, baseUrl:string) => {
+    params.set("page", page);
+    const li = `<li><a href="${baseUrl}?${params}">${content}</a></li>`;
+    return li;
+}
+
+const displayPaged = (total:number, limit:number, page, baseUrl:string) =>{
+    const qtyPages = Math.ceil(total / limit);
+    const pagesList = document.getElementById('pages');
+    const currentPage = page == undefined ? 1 : parseInt(page);
+    let listContent = '';
+
+    if(qtyPages === 0){
+        pagesList.innerHTML = '';
+        return;
+    }
+
+    if(currentPage != 1){
+        const back = getPageLi(currentPage - 1, "<", baseUrl);
+        listContent += back;
+        const first = getPageLi(1, "1", baseUrl);
+        listContent += first;
+    }
+
+    if(currentPage - 3 > 1){
+        const dots = `<li><span>...</span></li>`;
+        listContent += dots;    
+    }
+
+    if(currentPage - 2 > 1){
+        const prev_2 = getPageLi(currentPage - 2, `${currentPage - 2}`, baseUrl);
+        listContent += prev_2;    
+    }
+
+    if(currentPage - 1 > 1){
+        const prev_1 = getPageLi(currentPage - 1, `${currentPage - 1}`, baseUrl);
+        listContent += prev_1;    
+    }
+
+    const current = getPageLi(currentPage, `${currentPage}`, baseUrl);
+    listContent += current;
+
+    if(currentPage + 1 < qtyPages){
+        const next_1 = getPageLi(currentPage + 1, `${currentPage + 1}`, baseUrl);
+        listContent += next_1;
+    }
+
+    if(currentPage + 2 < qtyPages){
+        const next_2 = getPageLi(currentPage + 2, `${currentPage + 2}`, baseUrl);
+        listContent += next_2;
+    }
+
+    if(currentPage + 3 < qtyPages){
+        const dots = `<li><span>...</span></li>`;
+        listContent += dots; 
+    }
+
+    if(currentPage != qtyPages){
+        const last = getPageLi(qtyPages, `${qtyPages}`, baseUrl);
+        listContent += last;
+        const next = getPageLi(currentPage + 1, ">", baseUrl);
+        listContent += next;
+    }
+
+    pagesList.innerHTML = listContent;
+};
+
+
+// ------------------DISPLAY SELECT ORDER BY-------------------------
+
+const buildOrderBySelectByType = (type:string)=>{
+    if (type === 'comics') {
+        orderSelect.innerHTML = '';
+        const opAz = document.createElement('option');
+        opAz.appendChild(document.createTextNode('A-Z'));
+        opAz.setAttribute('value', 'title');
+        orderSelect.appendChild(opAz);
+
+        const opZa = document.createElement('option');
+        opZa.appendChild(document.createTextNode('Z-A'));
+        opZa.setAttribute('value', '-title')
+        orderSelect.appendChild(opZa);
+
+        const opNewer = document.createElement('option');
+        opNewer.appendChild(document.createTextNode('Más nuevos'));
+        opNewer.setAttribute('value', '-focDate');
+        orderSelect.appendChild(opNewer);
+
+        const opOlder = document.createElement('option');
+        opOlder.appendChild(document.createTextNode('Más viejos'));
+        opOlder.setAttribute('value', 'focDate');
+        orderSelect.appendChild(opOlder);
+    }
+
+    if (type === 'characters') {
+        orderSelect.innerHTML = '';
+
+        const opAz = document.createElement('option');
+        opAz.appendChild(document.createTextNode('A-Z'));
+        opAz.setAttribute('value', 'name');
+        orderSelect.appendChild(opAz);
+
+        const opZa = document.createElement('option');
+        opZa.appendChild(document.createTextNode('Z-A'));
+        opZa.setAttribute('value', '-name');
+        orderSelect.appendChild(opZa);
+    }
+};
+
+const messageError = (message:string) =>{
+    const alertHtml = `
+    <div class="alert alert-dark" role="alert">
+        <h4 class="alert-heading">Ups!</h4>
+        <p>${message}</p>
+    </div>
+    `;
+    return alertHtml;
+}
